8dc5969ddf083e2d5e281b9c4103fa9fe829ac0e
Malu Logrado <logradomalu@gmail.com>
2025-10-20 20:27:46 -0300

    adicionando novas credenciais

--- 02_preco_cotas.py
+++ 02_preco_cotas.py
@@ -11,7 +11,6 @@ HTTP_PATH = os.getenv("HTTP_PATH")
 TOKEN_BRAPI = os.getenv("TOKEN_BRAPI")
 TICKERS = os.getenv("TICKERS")
 
-print(HOSTNAME)
 
 print("Extraindo cotações..." )
 fetcher = ExtractPreco(TICKERS, TOKEN_BRAPI)
